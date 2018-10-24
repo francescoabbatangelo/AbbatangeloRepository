b9913b922539f6daa638546dcbcb95f54974fbd3
francescoabbatangelo <francescoabbatangelo91@gmail.com>
2018-10-24 18:33:25 +0200

    Restyling Website in Bootstrap

--- ProgettoFinale/src/js/index.js
+++ ProgettoFinale/src/js/index.js
@@ -1,7 +1,9 @@
 var i;
 for (i = 1; i < 5; i++) {
     let str = "like" + i;
+
     document.getElementById(str).addEventListener("click", function () {
+        console.log(this);
         if (document.getElementById(str).className === 'myclass_btn134') {
             document.getElementById(str).className = 'myclass_btn2';
         } else {
@@ -15,3 +17,4 @@ var here = document.getElementById("here");
 here.addEventListener("click", function () {
     document.getElementById("cookie_div").style.display = 'none';
 });
+
